7dd8a11295f64ad25db6871ab43a8fc09a3cb528
ketkev <kdjoosten@gmail.com>
2022-12-01 06:34:23 +0100

    Day 1

--- Days/Day01.cs
+++ Days/Day01.cs
@@ -8,58 +8,41 @@ namespace AoC2022.Days
 {
     public sealed class Day01 : BaseDay
     {
-        private readonly List<long> _heights;
+        private readonly List<long> _elves;
 
         public Day01()
         {
-            var input = File.ReadAllText(InputFilePath);
-            _heights = input.Split('\n').Select(long.Parse).ToList();
+            var input = File.ReadAllLines(InputFilePath);
+
+            _elves = new();
+
+            var sum = 0L;
+            
+            foreach (var line in input)
+            {
+                if (line.Length == 0)
+                {
+                    _elves.Add(sum);
+                    sum = 0L;
+                }
+                else
+                {
+                    sum += long.Parse(line);
+                }
+            }
         }
 
         public override ValueTask<string> Solve_1()
         {
-            var count = 0L;
-            var previous = _heights.First();
-
-            foreach (var height in _heights)
-            {
-                if (height > previous)
-                    count++;
-
-                previous = height;
-            }
-
-            return new ValueTask<string>($"{count}");
+            return new ValueTask<string>($"{_elves.Max()}");
         }
-
-        private static long Calculate3SlidingWindow(IReadOnlyList<long> heights, int index)
-        {
-            var window = 0L;
-
-            for (var i = index; i < index + 3 && i < heights.Count && i > 0; i++)
-            {
-                window += heights[i];
-            }
-
-            return window;
-        }
-
+        
         public override ValueTask<string> Solve_2()
         {
-            var count = 0L;
-
-            for (var i = 0; i < _heights.Count; i++)
-            {
-                var window = Calculate3SlidingWindow(_heights, i);
-                var previousWindow = Calculate3SlidingWindow(_heights, i - 1);
-
-                if (window > previousWindow)
-                {
-                    count++;
-                }
-            }
-
-            return new ValueTask<string>($"{count}");
+            _elves.Sort();
+            _elves.Reverse();
+            
+            return new ValueTask<string>($"{_elves.Take(3).Sum()}");
         }
     }
 }
