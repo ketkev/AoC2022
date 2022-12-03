d4812aa29900c554f12a5a920e9c30e27f4935ff
ketkev <kdjoosten@gmail.com>
2022-12-03 14:30:12 +0100

    Refactor day 3

--- Days/Day03.cs
+++ Days/Day03.cs
@@ -3,17 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AoC2022.utils;
 using AoCHelper;
 
 namespace AoC2022.Days
 {
-    public enum State
-    {
-        FirstOnly,
-        SecondOnly,
-        Both
-    }
-    
     public sealed class Day03 : BaseDay
     {
         private readonly List<String> _input;
@@ -24,49 +18,16 @@ namespace AoC2022.Days
 
         public override ValueTask<string> Solve_1()
         {
-            List<Dictionary<char, State>> items = new();
-            
-            foreach (var line in _input)
-            {
-                var first = line.Substring(0, line.Length / 2);
-                var second = line.Substring(line.Length / 2);
-
-                var dict = new Dictionary<char, State>();
-                
-                foreach (var c in first)
-                {
-                    if (!dict.ContainsKey(c))
-                    {
-                        dict.Add(c, State.FirstOnly);
-                    }
-                }
-                
-                foreach (var c in second)
-                {
-                    if (dict.ContainsKey(c) && dict[c] == State.FirstOnly)
-                    {
-                        dict[c] = State.Both;
-                    }
-                    else if (!dict.ContainsKey(c))
-                    {
-                        dict.Add(c, State.SecondOnly);
-                    }
-                }
-
-                items.Add(dict);
-            }
-            
             var result = 0L;
             
-            foreach (var dict in items)
+            foreach (var bag in _input)
             {
-                foreach (var (c, state) in dict)
-                {
-                    if (state == State.Both)
-                    {
-                        result += calculateScore(c);
-                    }
-                }
+                var first = bag.Substring(0, bag.Length / 2);
+                var second = bag.Substring(bag.Length / 2);
+
+                var intersect = first.Intersect(second).First();
+
+                result += CalculateScore(intersect);
             }
 
             return new ValueTask<string>($"{result}");
@@ -74,52 +35,20 @@ namespace AoC2022.Days
         
         public override ValueTask<string> Solve_2()
         {
+
+            var result = 0L;
             
-            List<char> badges = new List<char>();
-            
-            foreach (var strings in _input.Chunk(3))
+            foreach (var (bagOne, bagTwo, bagThree, _) in _input.Chunk(3))
             {
-                var dict = new Dictionary<char, int>();
-                
-                var bags = strings.ToList();
+                var badge = bagOne.Intersect(bagTwo).Intersect(bagThree).First();
 
-                int i = 0;
-                foreach (var bag in bags)
-                {
-                    i += 1;
-                    
-                    foreach (var c in bag)
-                    {
-                        if (i == 1 && !dict.ContainsKey(c))
-                        {
-                            dict.Add(c, i);
-                        }
-                        
-                        else if (i != 1 && dict.ContainsKey(c))
-                        {
-                            if (dict[c] == i - 1)
-                            {
-                                dict[c] = i;
-                            }
-                        }
-                    }
-                }
-                
-                foreach (var (c, value) in dict)
-                {
-                    if (value == 3)
-                    {
-                        badges.Add(c);
-                    }
-                }
+                result += CalculateScore(badge);
             }
-
-            var result = badges.Select(calculateScore).Sum();
 
             return new ValueTask<string>($"{result}");
         }
 
-        private long calculateScore(char c)
+        private long CalculateScore(char c)
         {
             int ascii = c;
             
