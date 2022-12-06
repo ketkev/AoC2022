a8621f5765bccd0b0c4ad6e5217429284e833b72
ketkev <kdjoosten@gmail.com>
2022-12-06 20:59:43 +0100

    Prepare day 7

---
+++ Days/Day07.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AoC2022.utils;
+using AoCHelper;
+
+namespace AoC2022.Days
+{
+    public sealed class Day07 : BaseDay
+    {
+        private string _input;
+
+        public Day07()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            return new ValueTask<string>($"TODO");
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            return new ValueTask<string>($"TODO");
+        }
+    }
+}
